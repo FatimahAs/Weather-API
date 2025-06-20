e7df770f37aacfa7955c0317a409263e98d2297f
FATIMAH ASIRI <fatimahasiri@FATIMAHs-MacBook-Pro-2.local>
2025-06-20 13:22:22 +0300

    test history

--- src/controllers/Weather.Controller.ts
+++ src/controllers/Weather.Controller.ts
@@ -7,25 +7,20 @@ export const getWeather = async (req: Request, res: Response): Promise<void> => 
   try {
     const userId = (req as any).user.userId;
     const { lat, lon } = req.query;
-console.log('h1')
     const latNum = parseFloat(lat as string);
     const lonNum = parseFloat(lon as string);
-console.log('h2')
     const roundedLat = parseFloat(latNum.toFixed(2));
     const roundedLon = parseFloat(lonNum.toFixed(2));
-console.log('h3')
     let weather = await Weather.findOne({ lat: roundedLat, lon: roundedLon });
 
     if (!weather) {
       const data = await FetchWeather(latNum, lonNum);
       weather = await Weather.create({ lat: roundedLat, lon: roundedLon, data });
     }
-console.log('h4')
     await History.create({ user: userId, weather: weather._id, lat: latNum, lon: lonNum });
-console.log('h5')
     res.json(weather.data);
   } catch (err: any) {
     res.status(500).json({ error: 'Failed to get weather', details: err.message });
   }
-	console.log('h6')
+	
 };
